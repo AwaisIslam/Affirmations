2889aeb71eb4190c080250395590c606f6f8b711
AwaisIslam <awais4823@gmail.com>
2022-07-07 17:21:24 +0500

    Initial commit

---
+++ app/src/test/java/AffirmationsAdapterTests.kt
@@ -0,0 +1,28 @@
+import android.content.Context
+import com.example.affirmations.model.Affirmation
+import org.mockito.MockedConstruction
+import org.mockito.Mockito.mock
+import com.example.affirmations.R
+import com.example.affirmations.adapter.ItemAdapter
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class AffirmationsAdapterTests {
+
+    private val context = mock(Context::class.java)
+
+
+    @Test
+    fun adapter_size() {
+        val data = listOf(
+            Affirmation(R.string.affirmation1,R.drawable.image1),
+            Affirmation(R.string.affirmation2,R.drawable.image2)
+        )
+
+        val adapter = ItemAdapter(context,data)
+
+        assertEquals("Item adapter is not the correct size", data.size,adapter.itemCount)
+    }
+
+
+}
